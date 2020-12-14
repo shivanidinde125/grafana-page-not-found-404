c254ba9a6670b7e0834a298770a07c7856b5cc8e
shivanidinde125 <shivanidinde125@gmail.com>
2020-12-14 12:28:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Error from "./Error.js";
+
+const App = () =>  {
+   
+    return(
+        
+        <Error/>
+
+        
+    );
+    
+};
+   
+export default App;
